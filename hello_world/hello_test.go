677aa79410729a6cdc2bd0b572d0058c4e3a137f
Thomas DoyleEngler <tpdoyle87@gmail.com>
2024-04-10 19:48:11 -0400

    refactor

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -2,11 +2,24 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
+func TestHelloName(t *testing.T) {
     got := Hello("Thomas")
     want := "Hello, Thomas"
 
+    assertCorrectMessage(t, got, want)
+}
+
+func TestHelloWorld(t *testing.T) {
+    got := Hello("")
+    want := "Hello, World"
+
+    assertCorrectMessage(t, got, want)
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+    t.Helper()
     if got != want {
         t.Errorf("got %q want %q", got, want)
     }
 }
+
--- hello_world/hello_world.go
+++ hello_world/hello_world.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
     fmt.Println(Hello("Thomas"))
     }
 
 func Hello(s string) string {
-    return fmt.Sprintf("Hello, %s", s)
+    if s == "" {
+      return "Hello, World"
+    }
+    return englishHelloPrefix + s
 }
