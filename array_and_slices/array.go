84cb775b9c649855f64ffa8b6f3b6d14f6a4ae0c
Thomas DoyleEngler <tpdoyle87@gmail.com>
2024-04-10 22:39:59 -0400

    arrays and slices with tests

--- array_and_slices/array.go
+++ array_and_slices/array.go
@@ -1,8 +1,33 @@
 package main
 
+import "fmt"
+
 func Sum(i []int) (total int) {
 	for _, n := range i {
 		total += n
 	}
 	return
+}
+
+func SumAll(numsToSum ...[]int) []int {
+    var sums []int
+    for _, nums := range numsToSum {
+        sums = append(sums, Sum(nums))
+    }
+    return sums
+}
+
+func SumAllTails(tailsToSum ...[]int) (string, []int) {
+    var sums []int
+    for _, nums := range tailsToSum {
+        if len(nums) == 0 {
+            return errorNilSlice(), nil
+        }
+        sums = append(sums, Sum(nums[1:]))
+    }
+    return "", sums
+}
+
+func errorNilSlice() string {
+    return fmt.Sprintf("slice can't be empty")
 }
--- array_and_slices/array_test.go
+++ array_and_slices/array_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-// 	"fmt"
+	"slices"
 	"testing"
 )
 
@@ -13,6 +13,33 @@ func TestSum(t *testing.T) {
 
         if total != expected {
             t.Errorf("expected %d but got %d given %v", expected, total, numbers)
+        }
+    })
+
+    t.Run("total two arrays together at respective index", func(t *testing.T) {
+        summed := SumAll([]int{1,2}, []int{0,9})
+        expect := []int{3,9}
+
+        if !slices.Equal(summed, expect) {
+            t.Errorf("Expected %v but got %v", expect, summed)
+        }
+    })
+
+    t.Run("Running the SumAllTails function", func(t *testing.T) {
+        _, tails := SumAllTails([]int{1,2,3}, []int{1,2,3})
+        expect := []int{5, 5}
+
+        if !slices.Equal(tails, expect) {
+            t.Errorf("Expected %v but got %v", expect, tails)
+        }
+    })
+
+    t.Run("catching nil slices", func(t *testing.T) {
+        err, _ := SumAllTails([]int{}, []int{1,2,4})
+        expect := "slice can't be empty"
+
+        if err != expect {
+            t.Errorf("Expected empty slice to be caught but it was't")
         }
     })
 }
